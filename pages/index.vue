761aba67484d88b5cd0a37a1b18c6e6de06b0fa6
kehwar <erickkwr@gmail.com>
2022-04-18 17:06:58 -0500

    Add default page (WIP)

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,58 @@
 <template>
-    <span> Hello World! </span>
+    <v-app>
+        <v-app-bar app> Roll Turn for Catan </v-app-bar>
+        <v-main>
+            <v-container>
+                <p>Who will play?</p>
+                <div class="input">
+                    <v-text-field
+                        ref="textInput"
+                        v-model="nameInput"
+                        label="Player"
+                        outlined
+                        @keypress.enter="onEnter"
+                    >
+                    </v-text-field>
+                    <v-btn @click="onEnter">Enter</v-btn>
+                </div>
+                <div class="chip-group">
+                    <v-chip v-for="(name, index) in names" :key="index">
+                        {{ name }}
+                    </v-chip>
+                </div>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
 export default {
     name: 'IndexPage',
+    data() {
+        return {
+            nameInput: '',
+            names: [],
+        }
+    },
+    methods: {
+        onEnter() {
+            if (this.nameInput === '') return
+            this.names.push(this.nameInput)
+            this.nameInput = ''
+            this.$refs.textInput.focus()
+        },
+    },
 }
 </script>
+
+<style scoped>
+.input {
+    display: flex;
+    gap: 10px;
+}
+.chip-group {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+}
+</style>
