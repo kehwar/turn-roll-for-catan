b7e2510af7ed9006f685e56f780cb4bfbe60c92e
kehwar <erickkwr@gmail.com>
2022-04-29 13:24:23 -0500

    Change theme primary color

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,7 @@ export default {
             dark: false,
             themes: {
                 light: {
-                    primary: colors.blue.darken2,
+                    primary: '#F2C335',
                     accent: colors.grey.darken3,
                     secondary: colors.amber.darken3,
                     info: colors.teal.lighten1,
